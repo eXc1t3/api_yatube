eafdac7f0496f612fda080ad6c048d76cfb75a03
Orlan Uzum <uzumorlan@mail.ru>
2023-11-24 17:37:16 +0300

    rev2

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -3,8 +3,14 @@ from rest_framework import serializers
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(read_only=True, slug_field='username')
-    group = serializers.SlugRelatedField(read_only=True, slug_field='title')
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+    group = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='title'
+    )
 
     class Meta:
         fields = ('__all__')
@@ -12,8 +18,10 @@ class PostSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(read_only=True,
-                                          slug_field='username')
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
 
     class Meta:
         fields = '__all__'
